02a7f87585233e0f152dbaf7de4f0541cbd54d65
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2023-01-10 23:23:28 +0530

    secured key

--- OnlineJobPortal/settings.py
+++ OnlineJobPortal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from . import pvt 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'onxxe)(1(7*85n&xq5weqbvqnfl*q#1r3krukd0))0n$z!x#2y'
+SECRET_KEY = pvt.S_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
