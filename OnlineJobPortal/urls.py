af3ab0ed49b0b83189bdbadbaddad7556873e66a
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2023-02-14 09:48:53 +0530

    prob: added userauth route in urls py

--- OnlineJobPortal/urls.py
+++ OnlineJobPortal/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+import userauth.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('userauth', include('userauth.urls')),
 ]
