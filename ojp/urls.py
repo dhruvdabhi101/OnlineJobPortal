9b0c7479215536b012ab2d2fe4f2d81ef1d5f53d
Dhruv Dabhi <dhruvdabhi101@gmail.com>
2023-03-06 11:35:57 +0530

    re: changed login page

--- ojp/urls.py
+++ ojp/urls.py
@@ -4,16 +4,13 @@ from . import views
 urlpatterns = [
     # for user authentication
     path('index/',views.index,name='index'),
-    path('register-recruiter/', views.registerRecruiter, name='register-recruiter'),
-    path('register-job-seeker/', views.registerJobSeeker, name='register'),
     path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('forgorpassword/', views.forgotpassword, name='forgotpassword'),
-    
 
     # for applicant
+    path('register-jobseeker/', views.register_jobseeker, name='register-jobseeker'),
     
 
     # for recruiter
+    path('register-recruiter/', views.register_recruiter, name='register-recruiter'),
     
 ]
--- ojp/views.py
+++ ojp/views.py
@@ -5,26 +5,11 @@ from django.http import HttpResponse
 def index(request):
     return render(request, "index.html")
 
-def register(request):
-    # return render(request, 'userauth/register.html')
-    return render(request, 'userauth/signup.html') 
-
-
 def login(request):
     return render(request, 'userauth/login.html')
-    
 
-def logout(request):
-    # return render(request, 'userauth/logout.html')
-    return HttpResponse("Hello, world. You're at the userauth logout page.")
+def register_recruiter(request):
+    return render(request, 'userauth/register_recruiter.html')
 
-def forgotpassword(request):
-    # return render(request, 'userauth/forgotpassword.html')
-    return HttpResponse("Hello, world. You're at the userauth forgotpassword page.")    
-
-def registerRecruiter(request):
-    return render(request, 'userauth/registerRecruiter.html')
-
-def registerJobSeeker(request):
-    pass
-    # return render(request, 'userauth/registerJobSeeker.html')
+def register_jobseeker(request):
+    return render(request, 'userauth/register_jobseeker.html')
